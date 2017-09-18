4869e5c29295fa34fc9de53dc613ea6e6e116853
hailin.liu <hailin.liu@okcoin.com>
2017-09-18 21:22:51 +0800

    update

--- xmemcached-spring-boot-autoconfigure/src/main/java/com/spring/boot/xmemcached/XmemcacheConfig.java
+++ xmemcached-spring-boot-autoconfigure/src/main/java/com/spring/boot/xmemcached/XmemcacheConfig.java
@@ -1,7 +1,6 @@
 package com.spring.boot.xmemcached;
 
 import lombok.Data;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
